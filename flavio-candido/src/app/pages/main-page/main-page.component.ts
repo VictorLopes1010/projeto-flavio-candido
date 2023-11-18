7979330c1821b3a66376948ba53d7ad931a654b1
Vitim <vlinvictus@gmail.com>
2023-11-17 22:18:42 -0300

    Carousel finalizado e botão whatsapp quase acabado

--- flavio-candido/src/app/pages/main-page/main-page.component.ts
+++ flavio-candido/src/app/pages/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -7,12 +8,30 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class MainPage implements OnInit {
-
+  mensagem: string = ''
+  numero: string = "+55 31 99393-0770"
+  isMouseOver: boolean = false;
+  isMouseOverTxt: boolean = false;
   ngOnInit(): void {
 
   }
 
   constructor() {
 
+  }
+
+
+
+  construirLink(): string {
+    const mensagemCodificada = encodeURIComponent(this.mensagem);
+    return `https://api.whatsapp.com/send?phone=${this.numero}&text=${mensagemCodificada}`;
+  }
+  openTextArea(){
+    setTimeout(()=>{
+      this.isMouseOverTxt = true;
+  }, 600);
+  }
+  enviarParaWpp() {
+    window.open(this.construirLink(), '_blank');
   }
 }
